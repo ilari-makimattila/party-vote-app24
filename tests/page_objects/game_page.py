baa81e9f808283968242fd529cc57f9ea5c98a3c
Ilari Mäkimattila <ilari@reboot.fi>
2024-05-06 16:14:59 +0200

    Show join form on main game page

--- tests/page_objects/game_page.py
+++ tests/page_objects/game_page.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from fastapi.testclient import TestClient
 
 from voting24.game.game import Key
@@ -10,12 +10,24 @@ class GamePage:
         return cls(testclient.get(f"/game/{game_key}").text)
 
     def __init__(self, html: str) -> None:
+        print(html)
         self.html = BeautifulSoup(html, "html.parser")
         css = self.html.css
         assert css
         self.css = css
 
+    def title(self) -> str:
+        t = self.html.title
+        assert t
+        assert t.string
+        return t.string
+
     def name(self) -> str:
         h1 = self.css.select_one("h1")
         assert h1
         return str(h1.text)
+
+    def join_form(self) -> dict[str, Tag]:
+        form = self.css.select_one("#join-form")
+        assert form
+        return {inp.attrs["name"]: inp for inp in form.select("input")}
--- tests/unit/web/test_get_game_route.py
+++ tests/unit/web/test_get_game_route.py
@@ -7,3 +7,15 @@ from voting24.game.game import Game
 def main_game_page_should_contain_game_name(testclient: TestClient, game: Game) -> None:
     page = GamePage.open(testclient, game.key)
     assert page.name() == game.name
+
+
+def main_game_page_should_contain_game_name_in_the_title(testclient: TestClient, game: Game) -> None:
+    page = GamePage.open(testclient, game.key)
+    assert game.name in page.title()
+
+
+def main_game_page_should_contain_join_form(testclient: TestClient, game: Game) -> None:
+    page = GamePage.open(testclient, game.key)
+    form = page.join_form()
+    assert form["game_key"].attrs["value"] == game.key
+    assert form["player_name"]
